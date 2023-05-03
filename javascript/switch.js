8d661f9cf043b09017b6ae2bfd6e681024dfa1aa
Carlos Perren <idux.net@gmail.com>
2023-05-03 20:56:42 -0300

    Sintaxis alternativa switch

---
+++ javascript/switch.js
@@ -0,0 +1,38 @@
+/*
+Práctica con sintaxis alternativa switch case
+
+Qué sucede si por ejemplo necesitamos mostrar un determinado mensaje
+de acuerdo a un indicador que puede tomar 5 valores diferentes?.
+*/
+
+const campoResultado = document.getElementById('resultado');
+
+let indicador = 3;
+let txt_resultado = '';
+
+switch (indicador) {
+    case 1:
+        txt_resultado = 'Conectando';
+        break;
+    
+    case 2:
+        txt_resultado = 'Asignando';
+        break;
+    
+    case 3:
+        txt_resultado = 'Conectado';
+        break;
+
+    case 4:
+        txt_resultado = 'Desconectado';
+        break;
+
+    case 5:
+        txt_resultado = 'ERROR';
+        break;
+    
+    default:
+        txt_resultado = 'Código no válido';
+}
+
+campoResultado.innerHTML = '<i>' + txt_resultado + '</i>';

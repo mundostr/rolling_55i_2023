21fe73a4c465bfe6dfc1b17b1efc5419652f8016
Carlos Perren <idux.net@gmail.com>
2023-05-03 21:50:00 -0300

    Ejemplos ciclos de control

---
+++ javascript/ciclos.js
@@ -0,0 +1,32 @@
+/*
+Ejemplos de alternativas de ciclos
+Un ciclo no es más que un bloque de código que se repite de 3 posibles maneras:
+    1- infinita (mientras dure la ejecución del código)
+    2- indefinida (hasta que se verifique una determinada condición)
+    3- finita (una x cantidad de veces)
+*/
+
+// Ej 1: contar hasta límite
+// Iniciamos en cero contador para que los 3 ciclos debajo lo vayan incrementando
+
+const LIMITE = 10;
+const LIMITE2 = 20;
+let contador = 0;
+
+// va incrementando y mostrando por consola mientras contador no llegue a LIMITE
+while (contador < LIMITE) {
+    contador++; // contador = contador + 1
+    console.log(contador);
+}
+
+// ejecuta LIMITE veces
+for (let ciclo = 1; ciclo <= LIMITE; ciclo++) {
+    contador++;
+    console.log(contador);
+}
+
+// idem while, pero el do while nos asegura que el paquete de instrucciones del ciclo se ejecute al menos una vez
+do {
+    contador++;
+    console.log(contador);
+} while(contador < LIMITE2)

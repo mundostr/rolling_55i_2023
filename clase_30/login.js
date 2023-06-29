eaa57a16d2ccb30ebc3d4de8858f4feb5c0916cd
Carlos Perren <idux.net@gmail.com>
2023-06-29 10:03:01 -0300

    Agregado cuadro mensaje

--- clase_30/login.js
+++ clase_30/login.js
@@ -1,12 +1,14 @@
-// Generamos las referencias necesarias al formulario y los dos campos de datos
+// Generamos las referencias al formulario y el cuadro de mensaje
 const formulario = document.querySelector('form');
-const email = document.getElementById('email');
-const clave = document.getElementById('clave');
-
+const mensaje_estado = document.getElementById('mensaje_estado');
 
 // Quedamos en escucha del evento submit del formulario, que se produce en este caso
 // cuando el usuario pulsa sobre el botón Ingresar, ya que es de tipo submit
 formulario.addEventListener('submit', function (event) {
+    // Generamos las referencias a los dos campos de datos
+    const email = document.getElementById('email');
+    const clave = document.getElementById('clave');
+    
     // Esto previene que el navegador intente recargar la página, nos permite manejar
     // manualmente el procesamiento del formulario
     event.preventDefault();
@@ -29,19 +31,36 @@ formulario.addEventListener('submit', function (event) {
         if (usuario_valido) {
             window.location.href = './privada.html';
         } else {
+            // Mostramos el mensjae de error y pasamos el foco al campo email
+            mensaje_estado.innerHTML = 'Error en email y/o clave';
+            mensaje_estado.classList.remove('invisible');
+            mensaje_estado.classList.add('visible');
             email.focus();
+            ocultarMensaje();
         }
     } else {
+        mensaje_estado.innerHTML = 'Datos faltantes o no válidos';
+        mensaje_estado.classList.remove('invisible');
+        mensaje_estado.classList.add('visible');
         email.focus();
+        ocultarMensaje();
         console.log('Formulario ERROR');
     }
 });
 
 // Esta función es solo para práctica, lógicamente esta línea única podría colocarse
 // directamente donde llamamos a la función.
-function recuperarUsuarios() {
+function recuperarUsuarios () {
     // El operador || nos permite retornar un array vacío si no se encuentra lista_usuarios en el localStorage
     return JSON.parse(localStorage.getItem('lista_usuarios')) || [];
+}
+
+function ocultarMensaje () {
+    setTimeout(() => {
+        // Luego de 3 segundos ocultamos el cuadro de mensaje
+        mensaje_estado.classList.remove('visible');
+        mensaje_estado.classList.add('invisible');
+    }, 3000);
 }
 
 

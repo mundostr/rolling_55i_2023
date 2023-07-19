cc8528f21e0a3d1f1fbe7d1986e825620aacdd20
Carlos Perren <idux.net@gmail.com>
2023-07-19 11:04:51 -0300

    Carrito base

---
+++ carrito/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+import Producto from './Producto.jsx';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Badge from 'react-bootstrap/Badge';
+
+import { BsFillCartPlusFill } from "react-icons/bs";
+
+function App() {
+  const URL_API = 'http://pad19.com:3050/';
+
+  const [productos, setProductos] = useState([]);
+  const [carrito, setCarrito] = useState({ productos: [], totalItems: 0, totalImporte: 0 });
+
+  const handleCarrito = (producto, cantidad) => {
+    const nuevoProducto = { producto: producto, cantidad: cantidad };
+    setCarrito({
+      productos: [
+        ...carrito.productos,
+        nuevoProducto
+      ],
+      totalItems: carrito.totalItems + cantidad,
+      totalImporte: carrito.totalImporte + producto.precio * cantidad
+    });
+  }
+
+  const handleCheckOut = () => {
+    if (carrito.productos.length === 0) {
+      alert('Nada en el carrito!');
+    } else {
+      console.log(carrito);
+    }
+  }
+
+  useEffect(() => {
+    const recuperarProductos = async () => {
+      const resultado = await fetch(URL_API)
+      const resultadoJson = await resultado.json();
+      setProductos(resultadoJson);
+    }
+
+    recuperarProductos();
+
+    return () => {}
+  }, [])
+
+  return (
+    <>
+      <div style={{ position: 'fixed', top: '1em', right: '1em', zIndex: 1000 }}>
+        <Button variant="success" onClick={handleCheckOut} style={{ float: 'right', fontSize: '150%' }}>
+          <BsFillCartPlusFill style={{ fontSize: '150%', marginBottom: '0.25em' }} /><br/>
+          <Badge bg="danger" style={{ marginRight: '0.5em' }}>{carrito.totalItems}</Badge>
+          ${carrito.totalImporte}
+        </Button>
+      </div>
+
+      <Container className="p-3">
+        <Container className="p-3 mb-3 text-white bg-dark rounded-3">
+          <h1>Rolling Compras</h1>
+        </Container>
+
+        <h3>ELECTRODOMESTICOS</h3>
+
+        {
+          productos.map(producto =>
+            <Producto key={producto._id} datos={producto} handleCarrito={handleCarrito} />
+          )
+        }
+      </Container>
+    </>
+  )
+}
+
+export default App
---
+++ carrito/src/Producto.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import { BsFillCartPlusFill } from "react-icons/bs";
+
+function Producto({ datos, handleCarrito }) {
+  const [cantidad, setCantidad] = useState(1);
+
+  const handleCantidad = (event) => {
+    const cantidad = Number(event.target.value);
+    if (cantidad > 0 && cantidad <= datos.stock) setCantidad(cantidad);
+  }
+
+  return (
+    <>
+      <Card className="mb-3" style={{width: '100%'}}>
+        <Card.Body>
+          <Card.Img variant="top" src={datos.imagen} style={{width: '200px', float: 'left', marginRight: '0.5em'}} />
+          <Card.Title>{datos.nombre}</Card.Title>
+          <Card.Text>
+            <span>{datos.codigo}</span><br />
+            <span><b>{`$ ${datos.precio.toFixed(2)}`}</b></span><br />
+            <span>{datos.stock}</span> en stock
+          </Card.Text>
+          
+          <Form.Control type="number" value={cantidad} onChange={handleCantidad} style={{float: 'left', width: '4em', marginRight: '0.5em'}} />
+          
+          <Button variant="success" onClick={() => handleCarrito(datos, cantidad)}>
+            <BsFillCartPlusFill style={{marginRight: '0.5em'}} />
+            Agregar
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
+  )
+}
+
+export default Producto
